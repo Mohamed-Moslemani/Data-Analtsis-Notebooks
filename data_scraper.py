8971d58151bf00a4ad7bc0df84f8923eb7a21d32
Mohamed Moslemani <moslemanomohamed@gmail.com>
2024-10-17 23:57:12 +0300

    text blob sentiment analysis

--- data_scraper.py
+++ data_scraper.py
@@ -4,8 +4,9 @@ from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.action_chains import ActionChains
 import time
+import pandas as pd  
 
-chromedriver_path = "C:\chromedriver-win64\chromedriver.exe"
+chromedriver_path = r"C:\chromedriver-win64\chromedriver.exe"
 
 options = webdriver.ChromeOptions()
 options.add_argument('--headless')  
@@ -14,20 +15,24 @@ options.add_argument('--no-sandbox')
 
 driver = webdriver.Chrome(service=Service(chromedriver_path), options=options)
 
-def scrape_youtube_comments(video_url, num_scrolls=5):
+def scrape_youtube_comments(video_url, num_scrolls=200):  
     driver.get(video_url)
     time.sleep(5)
-    action = ActionChains(driver)
+    action = ActionChains(driver) 
     for _ in range(num_scrolls):
         action.send_keys(Keys.PAGE_DOWN).perform()
-        time.sleep(2) 
+        time.sleep(2)  
     comment_elements = driver.find_elements(By.XPATH, '//*[@id="content-text"]')
     comments = [comment.text for comment in comment_elements]
     return comments
 
-video_url = 'https://www.youtube.com/watch?v=snX5YyflrGw'
+video_url = 'https://www.youtube.com/watch?v=Pv0iVoSZzN8'
 comments = scrape_youtube_comments(video_url)
+
 for i, comment in enumerate(comments):
     print(f"{i+1}: {comment}")
+
+df_comments = pd.DataFrame(comments, columns=["Comment"])
+df_comments.to_csv("youtube_comments.csv", index=False)
 
 driver.quit()
